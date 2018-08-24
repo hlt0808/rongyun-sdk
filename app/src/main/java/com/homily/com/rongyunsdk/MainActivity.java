450882940b11b5425bbe366447c892942cbff294
zhanglele <zhangtwo3254@163.com>
2018-08-24 19:58:13 +0800

    1.添加IMKit jitpack配置；

--- app/src/main/java/com/homily/com/rongyunsdk/MainActivity.java
+++ app/src/main/java/com/homily/com/rongyunsdk/MainActivity.java
@@ -1,9 +1,20 @@
 package com.homily.com.rongyunsdk;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created By User: 肖盼伟
  * Date: 2018/8/24
  * description :
  */
-public class MainActivity {
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
 }
